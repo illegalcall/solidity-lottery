3c240a893ad0d77502b2db6955885586849e58aa
Dhruv Sharma <dhruvsharma@cult>
2023-06-13 17:25:53 +0530

    smart contract deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,25 @@
-require('@nomiclabs/hardhat-waffle')
-require('dotenv').config()
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
 
 module.exports = {
-  defaultNetwork: 'localhost',
-  networks: {
-    hardhat: {},
-    localhost: {
-      url: 'http://127.0.0.1:8545',
-    },
-  },
-  solidity: {
-    version: '0.8.17',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-  mocha: {
-    timeout: 40000,
-  },
-}
+	defaultNetwork: 'localhost',
+	networks: {
+		hardhat: {},
+		localhost: {
+			url: 'http://127.0.0.1:8545',
+		},
+	},
+	solidity: {
+		version: '0.8.17',
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	mocha: {
+		timeout: 40000,
+	},
+};
